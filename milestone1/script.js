5625674308266686c7f4cf46bd8204bccc6e4e09
ShaguftaKanwal786 <161942642+ShaguftaKanwal786@users.noreply.github.com>
2024-09-29 01:49:47 +0500

    Add files via upload

---
+++ milestone1/script.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const toggleButton = document.getElementById('toggle-skills');
+const skill = document.getElementById('skill');
+toggleButton.addEventListener('click', () => {
+    if (skill.style.display === 'none') {
+        skill.style.display = 'block';
+    }
+    else {
+        skill.style.display = 'none';
+    }
+});
